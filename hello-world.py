2ca61f9010380757dcf3bee79f3976c8d999c5bb
Tahir Iqbal <info.tahiriqbal@gmail.com>
2021-08-27 13:11:53 +0500

    Say, Hello World!

---
+++ hello-world.py
@@ -0,0 +1,9 @@
+""" This file is create and managed by Tahir Iqbal
+    ----------------------------------------------
+       It can be use only for education purpose
+"""
+
+# print is pre-defined built-in python function to display given parameter
+print("Hello World!")
+print("Welcome to my repository")
+print("Tap on star and fork to share & help to others")
